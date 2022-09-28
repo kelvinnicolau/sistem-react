73d03812b1b429b2f62cd989089186ea0079f529
Kelvin <kelvinnicolau7@gmail.com>
2022-09-27 23:54:02 -0300

    Login, cadastro e header pronto

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,19 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/auth";
+
+import Header from '../../components/Header';
+
 export default function Dashboard(){
+
+    const { signOut } = useContext(AuthContext);
+
     return(
         <div>
+            <Header/>
+
+
             <h1>Página de Dashboard</h1>
+            <button onClick={ () => signOut() }>Fazer Logout</button>
         </div>
     )
 }
