fe0f16a76149d6ea82a3514c706921c9dcf1786e
Kelvin <kelvinnicolau7@gmail.com>
2022-10-01 23:51:09 -0300

    new cadastro ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 import Customers from '../pages/Customers';
+import New from '../pages/New';
 
 export default function Routes(){
     return(
@@ -16,9 +17,11 @@ export default function Routes(){
             <Route exact path="/" component={SignIn} />
             <Route exact path="/register" component={SignUp} />
 
+
             <Route exact path="/dashboard" component={Dashboard} isPrivate />
             <Route exact path="/profile" component={Profile} isPrivate />
             <Route exact path="/customers" component={Customers} isPrivate />
+            <Route exact path="/new" component={New} isPrivate />
         </Switch>
     )
 }
